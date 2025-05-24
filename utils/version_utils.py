64923c74d2bd6750b249f141a5dfae0934b11324
Sygaro <reidarg@gmail.com>
2025-05-24 22:01:08 +0200

    new file:   "config/frontend_version.json\357\200\250" 	new file:   status/bootstrap_status.json 	new file:   utils/version_utils.py

---
+++ utils/version_utils.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def get_git_version():
+    try:
+        result = subprocess.check_output(["git", "describe", "--tags", "--always"], stderr=subprocess.DEVNULL)
+        return result.decode("utf-8").strip()
+    except Exception:
+        return "ukjent"
