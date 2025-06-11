2f19638b62a7e69addeb9d3bcce34cc934283c78
Sygaro <reidarg@gmail.com>
2025-06-11 12:40:29 +0200

    Opprydding

--- tools/test_logger.py
+++ tools/test_logger.py
@@ -1,16 +1,12 @@
 from utils.logging.unified_logger import get_logger
 
 def main():
-    logger1 = get_logger("GarageController", category="port_activity")
-    logger2 = get_logger("system_init", category="system")
-    logger3 = get_logger("env_manager", category="environment")
+    logger = get_logger("GarageController", category="system", source="API")
+    logger.info("Dette er en vanlig INFO-melding")
+    logger.change("Dette er en custom CHANGE-melding (med farge)")
+    logger.notice("Dette er en custom NOTICE-melding (med farge)")
+    logger.timing("Dette er en custom NOTICE-melding (med farge)")
 
-    logger1.debug("Port debug-melding (port_activity).")
-    logger1.info("Port åpnet (port_activity).")
-    logger2.info("Systeminit startet.")
-    logger2.error("Noe gikk galt i systeminit.")
-    logger3.info("Sensorer lastet.")
-    logger3.warning("Miljøsensor viser lav verdi.")
 
 if __name__ == "__main__":
     main()
